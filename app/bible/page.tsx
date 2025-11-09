26b2b036ed54fdbcbd8f8427a3019d0c28d83921
judavago1 <juan.vargasg@uniagustiniana.edu.co>
2025-11-09 03:20:15 +0000

    ultimo

---
+++ app/bible/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+export default function BiblePage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Bienvenido a la Biblia</h1>
+      <p>Selecciona un libro para comenzar.</p>
+    </div>
+  );
+}
